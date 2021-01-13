b096c310d9b0a7de689cb51e895eb1d0cc656b67
o0Ignition0o <jeremy.lempereur@gmail.com>
2021-01-13 11:12:57 +0100

    bump to bytes 1.0

--- src/http/multipart_stream.rs
+++ src/http/multipart_stream.rs
@@ -1,18 +1,17 @@
-use crate::http::GQLResponse;
-use crate::{QueryResponse, Result};
-use bytes::{buf::BufExt, Buf, Bytes};
+use crate::{http::GQLResponse, QueryResponse, Result};
+use bytes::{Buf, Bytes};
 use futures::{Stream, StreamExt};
 
 /// Create a multipart response data stream.
 pub fn multipart_stream(s: impl Stream<Item = Result<QueryResponse>>) -> impl Stream<Item = Bytes> {
     s.map(|res| serde_json::to_vec(&GQLResponse(res)).unwrap())
         .map(|data| {
-            Bytes::from(format!(
+            let mut b = Bytes::from(format!(
                 "\r\n---\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n",
                 data.len()
             ))
-            .chain(Bytes::from(data))
-            .to_bytes()
+            .chain(Bytes::from(data));
+            b.copy_to_bytes(b.remaining())
         })
         .chain(futures::stream::once(async move {
             Bytes::from_static(b"\r\n-----\r\n")
